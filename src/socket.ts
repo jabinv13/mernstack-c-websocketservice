cdcad3cab93e7c64486b0eb7bfb9e76fe244a1f6
jabin-v <jabinv03@gmail.com>
2024-10-24 16:05:04 +0530

    change cors

--- src/socket.ts
+++ src/socket.ts
@@ -1,9 +1,17 @@
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import config from "config";
 
 const wsServer = createServer();
 
-const io = new Server(wsServer, { cors: { origin: "http://localhost:5173" } });
+const ALLOWED_DOMAINS = [
+  config.get("frontend.clientUI"),
+  config.get("frontend.adminUI"),
+];
+
+const io = new Server(wsServer, {
+  cors: { origin: ALLOWED_DOMAINS as string[] },
+});
 
 io.on("connection", (socket) => {
   console.log("Client connected", socket.id);
